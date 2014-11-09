b499bcd0f870dc199303d1427c6e90341049d944
Dustin Woods <pyrozzcom@gmail.com>
2014-11-09 13:43:02 -0800

    Updated acronym

--- ga-content-experiments.php
+++ ga-content-experiments.php
@@ -11,7 +11,7 @@ Author URI: http://dustinwoods.net/
 
 /**
  * Compatibility Goal:
- * 	GAXM should be compatible with
+ * 	wpgacxm should be compatible with
  * 		Wordpress SEO (Yoast)
  * 		All custom post types
  * 		W3 Total Cache (or similar cache plugin)
@@ -21,18 +21,18 @@ Author URI: http://dustinwoods.net/
  */
 
 
-function gaxm_manage_posts_columns() {
+function wpgacxm_manage_posts_columns() {
   //Show flags in list for all registered post types ( so also custom posts )
   $post_types = get_post_types('','names');
-  $post_types = apply_filters( 'gaxm_manage_post_types', $post_types );
+  $post_types = apply_filters( 'wpgacxm_manage_post_types', $post_types );
 
   foreach ($post_types as $type ) {
-    add_action( "manage_${type}_posts_custom_column", 'gaxm_admin_add_experiment_column', 10, 2);
-    add_filter( "manage_${type}_posts_columns" , 'gaxm_admin_add_experiment_columns' );
+    add_action( "manage_${type}_posts_custom_column", 'wpgacxm_admin_add_experiment_column', 10, 2);
+    add_filter( "manage_${type}_posts_columns" , 'wpgacxm_admin_add_experiment_columns' );
   }
 }
 
-function gaxm_admin_add_experiment_columns( $columns ) {
+function wpgacxm_admin_add_experiment_columns( $columns ) {
 	$columns['experiments'] = "Experiments";
 	return $columns;
 }
@@ -41,8 +41,8 @@ function gaxm_admin_add_experiment_columns( $columns ) {
 /*
  * add flags to single item
  */
-function gaxm_admin_add_experiment_column( $col_name, $id ) {
+function wpgacxm_admin_add_experiment_column( $col_name, $id ) {
   if( $col_name !== "experiments" ) return;
   echo "No experiments";
 }
-add_action( 'admin_init', 'gaxm_manage_posts_columns', 10 );
+add_action( 'admin_init', 'wpgacxm_manage_posts_columns', 10 );
